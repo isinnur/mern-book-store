36b0064929e93ece17c99b07486ff666ca426e89
isinnur <isinngnyy@gmail.com>
2024-03-12 19:13:49 +0300

    feat: update a book

--- backend/index.js
+++ backend/index.js
@@ -80,6 +80,30 @@ app.get("/books/:id", async (request, response) => {
   }
 });
 
+//route for update a book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 //mongoDB bağlantısı ve express uygulamasının dinlenmesi
 mongoose
   .connect(mongoDBURL)
