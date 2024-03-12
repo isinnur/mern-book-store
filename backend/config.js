09bcd2c95cab1600fcca60fc40bba00f55df263d
isinnur <isinngnyy@gmail.com>
2024-03-12 11:30:52 +0300

    feat: add mongoDB and mongoose to node.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://isinngnyy:OVIomLUY712HXdgd@mern-book-store.lspibc7.mongodb.net/?retryWrites=true&w=majority&appName=mern-book-store";
+//password: OVIomLUY712HXdgd
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+
+//"/" (kök) yoluna yapılan GET isteklerini ele alır
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To Mern Stack Tutorial");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database ");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
