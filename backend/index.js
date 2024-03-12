3fba6e30fdfa29d48614bd7bbd6c2cf8dae5fc75
isinnur <isinngnyy@gmail.com>
2024-03-12 14:26:48 +0300

    feat: save a new book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-//"/" (kök) yoluna yapılan GET isteklerini ele alır
+// Middleware for parsing request body  --->>postman
+app.use(express.json());
+
+// "/" (kök) yoluna yapılan GET isteklerini ele alır
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome To Mern Stack Tutorial");
+});
+
+//route for save a new book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
